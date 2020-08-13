1d4b217357bea93cb7033a0ef550d2b1d07ad73d
Morgan Fula <morgan.fula@me.com>
2020-08-13 22:45:16 +0200

    adding style to index

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,34 @@ Ingredient.create(name: "white egg")
 Ingredient.create(name: "himbeer")
 Ingredient.create(name: "cherry")
 Ingredient.create(name: "soda")
+
+# require "json"
+# require "open-uri"
+# ​
+# puts "Cleaning database..."
+# Cocktail.destroy_all
+# puts "Creating cocktails..."
+# INGREDIENTS_URL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=gin"
+# result = JSON.parse(open(INGREDIENTS_URL).read)
+# ​
+# result["drinks"].each do |data|
+#   new_cocktail = Cocktail.new(name: data["strDrink"],
+#     img_url: data["strDrinkThumb"])
+#   new_cocktail.save!
+# ​
+#   i = 1
+#   until i > 15 do
+#     ingredient_id = Ingredient.find_by(name: data["strIngredient#{i}"])&.id
+#     if ingredient_id.present?
+#       dose = Dose.new(description: data["strMeasure#{i}"],
+#         cocktail_id: new_cocktail.id,
+#         ingredient_id: ingredient_id
+#       )
+#       dose.save
+#     end
+# ​
+#     i += 1
+#   end
+# ​
+# end
+# puts "Finished!"
